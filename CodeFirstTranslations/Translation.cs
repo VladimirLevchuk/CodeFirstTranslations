90456b845810457763acd5e872ee982f44965452
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2016-10-02 12:27:26 +0300

    a small piece of documenting comments

--- CodeFirstTranslations/Translation.cs
+++ CodeFirstTranslations/Translation.cs
@@ -19,20 +19,39 @@ namespace CodeFirstTranslations
             AlternativeKeys = alternativeKeys.OrEmpty().Select(TranslationContext.Current.Environment.PathUtil.MakeKey).ToList();
         }
 
+        /// <summary>
+        /// Value is a translation text, translated to the current culture (see TranslationContext.Current.CurrentCulrure
+        /// </summary>
         [NotNull]
         public virtual string Value => Translate(TranslationContext.Current);
+        /// <summary>
+        /// Translation key (primary)
+        /// </summary>
         [NotNull]
         public virtual string Key { get; }
+        /// <summary>
+        /// Alternative translation keys
+        /// </summary>
         [NotNull]
         public virtual List<string> AlternativeKeys { get; }
+        /// <summary>
+        /// Default code-first value is used as a fallback for all cultures translation service cannot find translation for
+        /// </summary>
         [NotNull]
         public virtual string DefaultValue { get; }
 
+        [NotNull]
         public override string ToString()
         {
             return Value;
         }
 
+        /// <summary>
+        /// Returns code first value for a given culture. 
+        /// When currentCulture is null, or text for the culture is not defined in code Default value is returned
+        /// </summary>
+        /// <param name="currentCulture"></param>
+        /// <returns></returns>
         [NotNull]
         public virtual string GetCodeFirstValueFor([CanBeNull] string currentCulture)
         {
@@ -41,6 +60,12 @@ namespace CodeFirstTranslations
 
             return _data.TryGetValue(currentCulture) ?? DefaultValue;
         }
+        /// <summary>
+        /// Defines a code-first translation with the given text for the culture specified
+        /// </summary>
+        /// <param name="culture"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
         [NotNull]
         public Translation AddTranslation([NotNull] string culture, [NotNull] string text)
         {
@@ -56,7 +81,12 @@ namespace CodeFirstTranslations
             return _data.ToDictionary(x => x.Key, x => x.Value, StringComparer.InvariantCultureIgnoreCase);
         }
 
-        protected virtual string Translate(IReadOnlyTranslationContext context)
+        /// <summary>
+        /// Translates the current translation text to the current culture taken from translation context
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        protected virtual string Translate([NotNull] IReadOnlyTranslationContext context)
         {
             if (context == null) throw new ArgumentNullException(nameof(context));
 
